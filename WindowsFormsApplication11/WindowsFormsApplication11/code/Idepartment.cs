d1f2c642e98ae9f3238c69d9f1884fffd3610fae
marwaalfarsi <121227930+marwaalfarsi@users.noreply.github.com>
2022-12-27 00:29:50 +0300

    First Commit

---
+++ WindowsFormsApplication11/WindowsFormsApplication11/code/Idepartment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication11.code
+{
+    interface Idepartment
+    {
+        public void insert(string name, string code);
+        public void Edite(int id, string name, string code);
+
+        public bool isex_name(string name);
+        public bool isex_code(string code);
+
+        public DataTable Getall();
+        public System.Data.DataRow Getbyid(int id);
+
+
+        public void Delete(int id);
+
+        public bool isusedinstudent();
+        public bool isusedincourse();
+
+    }
+}
---
+++ WindowsFormsApplication11/WindowsFormsApplication11/code/con12.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication11.code
+{
+    class con12
+    {
+        static string connentionstr = "";
+
+    }
+}
